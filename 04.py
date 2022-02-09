5afdc6da25e9d34a378f324ce22e02e5668cc92d
JassOunapuu <98941717+JassOunapuu@users.noreply.github.com>
2022-02-09 13:58:22 +0200

    Add files via upload

--- 04.py
+++ 04.py
@@ -3,9 +3,114 @@
 # 03.02.2022
 
 
-#juubel
-sunna = input("Sisesta oma sünnipäev:  ")
-paev,kuu,aasta = sunna.split(".")
+# Arvamismäng
+
+
+
+
+
+
+'''
+#viisikud
+
+for i in range(1,101):
+    if i%5 == 0:
+        print(i)
+        
+
+
+
+
+# Korrutustabel
+
+for i in range(1,11):
+    a=5
+    v=i*a
+    print(f"{a} x {i} = {v}")
+
+print()
+
+
+
+
+
+
+# Paaris ja paaritu
+
+for i in range(1,101):
+    if i%2 == 0:
+        print(f"{i} paaris")
+    else:
+        print(f"{i} paaritu")
+
+
+# Loto
+
+from random import randrange
+for i in range(1,6):
+    print(randrange(0,9),end="")
+print()
+print()
+
+
+# Tärnid
+
+for i in range(1,6):
+    for j in range(1,6):
+        print('* ', end='')
+    print()
+print()
+
+for i in range(1,6):
+    print("* "* i)
+    
+print(" ")
+k = 6
+for t in range(1,6):
+    k = k - 1
+    print("* "* k)
+    
+'''
+
+# Jalgpalli meeskonna valimine
+
+sugu = input("Sisestage oma sugu(M/K/N):  ")
+
+if sugu.lower() == "m":
+    vanus = int(input("Sisestage vanus:  "))
+    if vanus >= 16 and vanus <= 18:
+        print("youre in")
+    else:
+        print("no luck, bye")
+else:
+    print ("Sa ei kuulu siia")
+
+
+
+
+# müük
+
+hind = int(input("Sisesta toote hind, et saada soodust: "))
+if hind <= 10:
+    print ("Toote allahindlus on 10%")
+    ale = 0.1
+else:
+    print ("Toote allahindlus on 20%")
+    ale = 0.2
+summa = hind - hind * ale
+
+print (f"maksmisele kulub {summa} eurot")
+
+
+
+#Juubel
+synd = input("Sisesta sünniaeg kujul dd.mm.yyyy: ")
+p, k, a = synd.split(".")
+vanus = 2022 - int(a)
+if vanus%5 == 0:
+    print("On juubel")
+else:
+    print("Ei ole juubel")
 
 
 
@@ -31,10 +136,10 @@ a = int(input("Sisesta ruudu üks külg:  "))
 b = int(input("Sisesta ruudu teine külg:  "))
 if a == b:
     print("Tegemist on ruuduga")
-#elif:
-#    klsdjlaj
 else:
     print("Risttahukas")
+
+
 
 
 
