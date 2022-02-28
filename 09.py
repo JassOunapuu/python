241c19edd36d181e8db4b41c20b21c44eba536f5
JassOunapuu <98941717+JassOunapuu@users.noreply.github.com>
2022-02-28 13:51:44 +0200

    Add files via upload

---
+++ 09.py
@@ -0,0 +1,30 @@
+# Jass Õunapuu
+#  28.02.2022
+#    IT-21
+
+
+
+#kuupäevad
+import datetime
+date = datetime.datetime.now()
+
+print(date.strftime("%d. February %Y"))
+print()
+print(date.strftime("%d. Veebruar %Y"))
+
+#vanuse leidmine isikukoodi abil
+kood ="5020824642"  #pole tegelik isikukood
+aasta = int(kood[1]+kood[2])+2000
+kuu = int(kood[3]+kood[4])
+paev = int(kood[5]+kood[6])
+
+date2 = date.strftime("%Y,%m,%d")
+aa,mm,dd = date2.split(",")
+
+from dateutil.relativedelta import relativedelta
+start_date = datetime.datetime(aasta,kuu,paev)
+end_date = datetime.datetime(int(aa),int(mm),int(dd))
+difference_in_years = relativedelta(end_date, start_date).years
+print(difference_in_years)
+
+
