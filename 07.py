cbfede82c37da9cb852c99beeb3f4b268b6bbd09
JassOunapuu <98941717+JassOunapuu@users.noreply.github.com>
2022-02-28 12:54:34 +0200

    Add files via upload

--- 07.py
+++ 07.py
@@ -4,28 +4,52 @@
 
 
 # Ruumalad
-kujund=1
-number=5
-def ruumala(kujund,number):
-    kujund = int(input("Vali kujund\n 1) Kuup\n 2) Kera\n 3) Koonus\n 4) Silinder\n Sisesta soovitud kujundi number: "))
+
+import math
+
+def kuRuumala(a):
+    v=a**3
+    return v
+
+def keRuumala(a):
+    v=(4*math.pi*a**3)/3
+    return v
+
+def koRuumala(a,h):
+    v=(math.pi*a**2*h)/3
+    return v
+
+def siRuumala(r,h):
+    v=math.pi*r**2*h
+    return v
+
+
+number=0
+loop=0
+while loop == 0:
+    kujund = int(input("Vali kujund\n 1) Kuup\n 2) Kera\n 3) Koonus\n 4) Silinder\n 5) Lõpeta\n Sisesta soovitud number: "))
+
     if kujund==1:
-        number = int(input("Valisid kuubi. Sisesta kuubi serva pikkus:  "))
-        print(f"Kuubi ruumala on: {number*number*number}")
+        number = int(input("Valisid kuubi. Sisesta kuubi küljepikkus:  "))
+        print(f"Kuubi ruumala on: {kuRuumala(number)}")
+    
     elif kujund==2:
         number = int(input("Valisid kera. Sisesta kera raadius:  "))
-        print(f"Kera ruumala on: {4*(number*(number*3.14))}")
-        
+        print(f"Kera ruumala on: {keRuumala(number)}")
+    
     elif kujund==3:
         number = int(input("Valisid koonuse. Sisesta koonuse raadius:  "))
-        print(f"Koonuse ruumala on: {4*(number*(number*3.14))}")
-        
+        number2 = int(input("Sisesta koonuse kõrgus:  "))
+        print(f"Koonuse ruumala on: {koRuumala(number,number2)}")
+    
+    elif kujund==4:
+        number = int(input("Valisid silindri. Sisesta silindri raadius:  "))
+        number2 = int(input("Sisesta silindri kõrgus:  "))
+        print(f"Silindri ruumala on: {siRuumala(number,number2)}")
     else:
-        
-        arv = arv1 * arv2
-    return ruumala(1,6)
+        loop=1
 
-
-
+print()
 
 
 
