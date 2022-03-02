45c595c5ff8f9c2544955e97f9dd49330ebd5c16
JassOunapuu <98941717+JassOunapuu@users.noreply.github.com>
2022-03-02 09:16:58 +0200

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,25 @@
+#  Jass Õunapuu
+#   02.03.2022
+#     IT21
+
+import re
+
+
+
+
+#Leian IP'd
+
+ip=" "
+
+with open('nonsense.txt', 'r') as fail:
+    for line in fail:
+        if re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",line):
+            print(line,end="")
+
+#Leian paroolid
+
+with open('nonsense.txt', 'r') as fail:
+    for line in fail:
+        if re.findall("^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).*$",line):
+            print(line,end="")
+
